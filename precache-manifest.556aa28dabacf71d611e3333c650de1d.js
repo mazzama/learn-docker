ea4be7dace283f8f9ce64e5f7dbf3511063f2a79
Muhammad Azzam <mazzamabduljabbar@gmail.com>
2019-04-12 16:17:37 +0700

    Updates

---
+++ precache-manifest.556aa28dabacf71d611e3333c650de1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1118a02499c43349edb0",
+    "url": "/learn-docker/static/js/main.1118a024.chunk.js"
+  },
+  {
+    "revision": "12fa6fd15ee8a5fe5ad1",
+    "url": "/learn-docker/static/css/1.6b0f0282.chunk.css"
+  },
+  {
+    "revision": "12fa6fd15ee8a5fe5ad1",
+    "url": "/learn-docker/static/js/1.12fa6fd1.chunk.js"
+  },
+  {
+    "revision": "1c95dac44779471a9ce0",
+    "url": "/learn-docker/static/js/runtime~main.1c95dac4.js"
+  },
+  {
+    "revision": "74cc103f32ff7e9010bc6149ccc0974f",
+    "url": "/learn-docker/static/media/docker-logo-software.74cc103f.png"
+  },
+  {
+    "revision": "1c45b34029f52897821be48eb210bc9c",
+    "url": "/learn-docker/static/media/containers.1c45b340.jpeg"
+  },
+  {
+    "revision": "d1d5f25cb0843b6d3213b87c367fd04d",
+    "url": "/learn-docker/static/media/docker-container.d1d5f25c.png"
+  },
+  {
+    "revision": "d04f29b3ce0a5ee02ac78210b1bdd274",
+    "url": "/learn-docker/static/media/vm-vs-container.d04f29b3.png"
+  },
+  {
+    "revision": "b595b80b1dc39db2af4ffd6f0e8e8f55",
+    "url": "/learn-docker/static/media/architecture.b595b80b.svg"
+  },
+  {
+    "revision": "73e03908a1acd490ea05645b0a73d700",
+    "url": "/learn-docker/static/media/giphy.73e03908.webp"
+  },
+  {
+    "revision": "bb866e3bc454055b2f6b6d0997b66a91",
+    "url": "/learn-docker/index.html"
+  }
+];
